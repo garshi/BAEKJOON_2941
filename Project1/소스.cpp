eb28f6d52e4a747c6978a78a51ec444be53b2b00
garshi <90501021+garshi@users.noreply.github.com>
2022-01-18 23:15:57 +0900

    FINAL

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -5,45 +5,65 @@ int main(void) {
 	int result = 0;
 	char A[110];
 	fgets(A, sizeof(A), stdin);
-	
-
-	for (int i = 0; i <= strlen(A); i++) {
 
 
-		if (A[i] == 'c' && A[i + 1] == '=') {
+	for (int i = 0; i < strlen(A) - 1; i++) {
+
+
+
+		if (A[i] == 'c' && A[i + 1] == '=') {									//c=
 			result++;
-			i += 1;
+			i++;
+
+		}
+		else if (A[i] == 'c' && A[i + 1] == '-') {
+
+			result++;
+			i++;
 		}
 		else if ((A[i] == 'd' && A[i + 1] == 'z') && A[i + 2] == '=') {
 
 			result++;
 			i += 2;
+
 		}
 		else if (A[i] == 'd' && A[i + 1] == '-') {
 
 			result++;
-			i += 1;
+			i++;
+
 		}
 		else if (A[i] == 'l' && A[i + 1] == 'j') {
 			result++;
-			i += 1;
+			i++;
+
 		}
 		else if (A[i] == 'n' && A[i + 1] == 'j') {
 			result++;
-			i += 1;
+			i++;
 
 		}
 		else if (A[i] == 's' && A[i + 1] == '=') {
 			result++;
-			i += 1;
-	}
+			i++;
+
+		}
 		else if (A[i] == 'z' && A[i + 1] == '=')
 		{
 			result++;
-			i += 1;
+			i++;
+
+
 		}
-		else result++;
+		else {
+			result++;
+
+		}
+
+
+
 	}
+
 
 	printf("%d", result);
 
